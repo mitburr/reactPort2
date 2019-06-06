0bd8d6653bf3a526649ae123f15940e7f00451cb
Mitchell Burr <mitburr@gmail.com>
2019-06-06 14:10:59 -0500

    heroku cors solution

--- src/Components/Utils/API.js
+++ src/Components/Utils/API.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default{
     gitRepos: function (req, res) {
         //first we call the github api for all my repos
-        return axios.get("https://api.github.com/users/mitburr/repos").then(results => {
+        return axios.get("https://cors-anywhere.herokuapp.com/https://api.github.com/users/mitburr/repos").then(results => {
             //then we map the repos to an array with the information I want (pretty much just the link)
             return results.data.map(Element => {
                 let repoObj = {
